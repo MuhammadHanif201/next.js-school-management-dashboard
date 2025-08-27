70f33b21e739a86b3d3030c5f62c3f294a4f5685
MuhammadHanif201 <hanifmuhammad.ca@gmail.com>
2025-08-27 20:07:50 +0500

    completed school dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2">Homepage</h1>
+      <p>
+        If you are on <code>http://localhost:3000</code>, please go to{" "}
+        <a
+          href="/admin"
+          className="text-blue-600 underline hover:text-blue-800"
+        >
+          /admin
+        </a>{" "}
+        for the admin dashboard.
+      </p>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
